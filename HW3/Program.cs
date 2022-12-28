3c05223e3e3aea92b21cd39014565114d0b5ce47
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-28 23:56:30 +1100

    Решение ДЗ к семинару 3

---
+++ HW3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+/*System.Console.WriteLine("Введите число");
+string num = Console.ReadLine()!;
+
+if (num.Length < 5 || num.Length > 5 )
+{
+    System.Console.WriteLine("Введите пятизначное число");
+}
+else if (num[0] == num[4] || num[1] == num[3])
+{
+    System.Console.WriteLine($"Число {num} паландром");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} не паландром");
+}*/
+
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/*System.Console.WriteLine("Введите координату Х точки А");
+int x1 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки A");
+int y1 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите координату Z точки А");
+int z1 = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите координату Х точки B");
+int x2 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки B");
+int y2 = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Введите координату Z точки B");
+int z2 = Convert.ToInt32(System.Console.ReadLine());
+
+double result = Math.Round(Math.Sqrt(Math.Pow((x1 - y1 - z1), 2) + Math.Pow((x2 - y2 - z2), 2)), 3);
+
+System.Console.WriteLine($" Расстояние между двумя точками: {result}");*/
+
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+/*System.Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}*/
+
+
