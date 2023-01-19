9e746d41ca2a4a75f360abcc8ca5134cff461ab6
Konnikov_Denis <dkonnikov.dk@mail.ru>
2023-01-19 23:16:37 +1100

    Доделал 4 ДЗ

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -4,21 +4,21 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-// Console.WriteLine("Введите число А:");
-// int A = int.Parse(Console.ReadLine()!);
-// Console.WriteLine("Введите число B:");
-// int B = int.Parse(Console.ReadLine()!);
-// int result = Ndegree(A,B);
-// Console.WriteLine($"Число А в натуральной степени B равна {(result)}");
-// //------Method------
+/*Console.WriteLine("Введите число А:");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B:");
+int B = int.Parse(Console.ReadLine()!);
+int result = Ndegree(A,B);
+Console.WriteLine($"Число А в натуральной степени B равна {(result)}");
+//------Method------
 
-// int Ndegree (int A, int B){
-//     int degree=1;
-//     for(int i=1; i <=B; i++) {
-//         degree = degree*A;
-//     }
-// return degree;
-// }
+int Ndegree (int A, int B){
+    int degree=1;
+    for(int i=1; i <=B; i++) {
+        degree = degree*A;
+    }
+return degree;
+}*/
 
 
 
@@ -27,42 +27,20 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.WriteLine("Введите число");
+/*Console.WriteLine("Введите число");
 int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"сумма всех цифр в числе = {Sum(A)}");
+Console.WriteLine($"сумма всех цифр в числе = {GetQuantity(A)}");
 // //----Method
 
-int Sum(int A)
-{
-    if ((uint)A.ToString().Length == 2)
-    {
-        int sum1 = (A / 10) + A % 10;
-    }
-
-    if ((uint)A.ToString().Length > 2)
-    {
-        int sum1 = ((A / 100) + ((A % 100) * 10) + A % 10); 
-    }
-    return sum1;
+int GetQuantity(int A){
+    int sum = 0;
+for (int i=0; i<=A;i++){
+    
+    sum = sum + A%10;
+    A=A/10;
 }
-
-// Console.WriteLine("Введите число");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"сумма всех цивр в числе = {(A%100)/10}");
-
-
-/*Console.WriteLine("введите число:");
-string number = Console.ReadLine()!;
-Console.Write($"Количество чисел равна {GetQuantity(number)} ");
-
-// ---------method---------
-
-int GetQuantity(string A){
-    int Quantity = A.Length;
-    return Quantity;
-}
-*/
-
+return sum;
+}*/
 
 //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
 
