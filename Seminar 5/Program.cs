c0948b5f7fcd69a113edf72402973b3b76b79594
Konnikov_Denis <dkonnikov.dk@mail.ru>
2023-01-22 15:09:04 +1100

    Семинар 5 с решениями

--- Seminar 5/Program.cs
+++ Seminar 5/Program.cs
@@ -1,77 +1,169 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-/*int[] Array = GetArray(6);
-Console.WriteLine($"[{String.Join(",", Array)}]"!);
-int[] Array2 = Channge(Array);
-Console.WriteLine($"[{String.Join(",", Array2)}]"!);
+//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
 
-//-------Методы
+/* int[] array = GetArray(12, -9,9);
+Console.WriteLine($"[{String.Join(",", array)}]");
 
-int[] GetArray(int size){
-    int [] res = new int[size];
-    for(int i=0; i<size; i++){
-        ReferenceEquals[i] = new Random().Next(-1000,1000);
+int positiveSum = 0;
+int negativeSum = 0;
 
-    }
-return res;
-}
- 
-int [] Channge(int[] Array)
+foreach (int element in array)
 {
-    foreach(int item = 0; i<Array.Length; i++)
-{
-    Array[item]=  -Array[item];
+    if (element > 0){
+        positiveSum =  positiveSum + element;
+    }
+    else {
+        negativeSum = negativeSum + element;
+    }
 }
-return Array;
-}*/
 
+Console.WriteLine($"сумма положительных равна {positiveSum}, сумма отрицательных равна {negativeSum}");
 
+// Метод (универсальный метод заполнения массива)
 
-/*int [] Array = GetArray(5);
-Console.WriteLine($"[{String.Join(",", Array)}]"!);
-Console.WriteLine("Введите число:");
-int number = int.Parse(Console.ReadLine()!);
-if(FindNumber(number, Array))
-{Console.WriteLine($"число {number} есть в массиве");}
-else 
-{Console.WriteLine($"число {number} нет в массиве");}
-
-//-----Methood
-
-int[] GetArray(int size){
-    int [] res = new int[size];
-    for(int i=0; i<size; i++){
-    res[i] = new Random().Next(20);
-
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] res = new int[size];
+    for (int i = 0; i<size; i++){
+        res[i] = new Random().Next(minValue, maxValue+1);
     }
-return res;
+    return res;
 }*/
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-/*bool FindNumber(int number, int[]Array){
-    foreach(int i in Array){
-        if(i==number)
-        return true;
-        
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] // массив не менее 5ти символов
+
+
+/*int[] Array = GetArray(30);
+Console.WriteLine($"[{String.Join(", ", Array)}]");
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = Array[i] * (-1);
+}
+
+Console.WriteLine($"[{String.Join(", ", Array)}]");
+
+// Method 
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
     }
-return false;
+    return array;
+}
+*/
 
-}*/
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-/*int FindNumber(int number, int[]Array){
-    for(int i=0; i <Array.Length; i++){
-        if(Array[i]==number)
-        return Array[i];
+//Задайте массив. Напишите программу, которая определяет,присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+// В этом решении проверяется каждая строка и выводит в терминале "НЕ найдено"
+
+// int[] Array = GetArray(10); 
+
+// int find = -5;
+// foreach (int ArrayEl in Array)
+// {
+//     if (ArrayEl == find)
+//     {
+//         Console.WriteLine($"Число найдено");
+//     }
+//     else {
+//         Console.WriteLine($"Число НЕ найдено");
+//     }
+// }
+
+// Method1 
+
+// int[] GetArray(int size) // 
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(-10, 10);
+//     }
+//     return array;
+// }
+
+
+// Решение 2 через boll 
+
+// int[] Array = GetArray(10); 
+// int Find = -5;
+// Console.WriteLine($"[{String.Join(", ", Array)}]");
+
+// if (FindElement(Array, Find)){
+//     Console.WriteLine($"Число найдено");
+// }
+// else {
+//      Console.WriteLine($"Число  НЕ найдено");
+// }
+
+// bool FindElement (int[] Array, int Find){
+// foreach (int ArrayEl in Array)
+// {
+//     if (ArrayEl == Find){
+//         return true;
+//     }
+
+// }
+// return false;
+// }
+
+
+// // Method2 
+
+// int[] GetArray(int size) 
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(-10, 10);
+//     }
+//     return array;
+// }
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+
+int[] Array= GetArray(123);
+
+// Method
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
     }
-return false;
-}*/
+    return array;
+}
+
+Console.WriteLine($"[{String.Join(", ", Array)}]");
+int count = 0;
+for (int i = 0; i<=122; i++){
+
+if (Array[i]>=10 && Array[i]<100){
+    count = count +1;
+}
+}
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99] {count}");
 
 
 
-
-//Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].int[] GetArray(int size){
-    
-
-int[] Array = GetArray(123);
+/*int[] Array = GetArray(123);
 Console.WriteLine($"[{String.Join(",", Array)}]"!);
 Console.WriteLine($"Количество элементов от 10 до 99 в массиве {Quantity(Array)}");
 
@@ -92,4 +184,4 @@ int Quantity(int [] Array){
         {count++;}
     }
     return count;
-}
+}*/
